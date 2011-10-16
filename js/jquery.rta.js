012615c71dc67d5c99461d61202ae00866cd846e
web.sbelarus <web.sbelarus@5b96e37f-d5a1-6910-ffdc-0394f24dd846>
2011-10-16 22:29:42 +0000

    Refactoring. Publish method is not required. Subscribe registers the listener to the queue communicationBridge is suits more than eventMachine

--- js/jquery.rta.js
+++ js/jquery.rta.js
@@ -15,10 +15,6 @@ $.jsa.RealTimeWidgetAbstract = $.jsa.extend({
     name: 'RealTimeWidgetAbstract',
     init : function() {
        $.rta.subscribe(this, this.subscribeRta);
-       $.rta.publish(this); // Checking in 
-    },
-    publishRta: function(message) {
-        $.rta.publish(this, message);
     }
 }, $.jsa.WidgetAbstract);
 
@@ -28,40 +24,43 @@ $.rta = (function() {
         subscriber: [],
         getQuery : function() {
             return JSON.stringify(_private.queue);
+        },
+        subscribeListener : function(source, fn) {
+           $(source).unbind('rta-message').bind('rta-message', fn);
+        },
+        isSourceValidate : function(source) {
+             if (typeof source.name == 'undefined') {
+                throw 'Not found name property of the listener';
+             }
+             if (typeof source.module == 'undefined') {
+                throw 'Not found module property of the listener';
+             }
+        },
+        enqueue : function(source) {
+            if (_private.subscriber[source.name]) {
+                return false;
+            }
+            _private.subscriber[source.name] = source;
+            _private.queue.push({
+                sourceName : source.name,
+                module: source.module,
+                param : null
+            });
         }
     }
     return {
-      subscribe : function(source, fn) {
-           $(source).unbind('rta-message').bind('rta-message', fn);
-      },
-      publish : function(source, message) {
-          if (typeof source.name == 'undefined') {
-              throw 'Widget object must have name property';
-          }
-          if (typeof message == 'undefined') {
-              message = [];
-          }
-          if (_private.subscriber[source.name]) {
-              return false;
-          }
-          _private.subscriber[source.name] = source;
-          return _private.queue.push($.extend({
-              sourceName : source.name,
-              module: source.module,
-              param: null,
-              frequency: 0,
-              action: 'push'
-          }, message));
-          
-         
+      subscribe : function(source, fn) {           
+           _private.isSourceValidate(source);
+           _private.enqueue(source);
+           _private.subscribeListener(source, fn)
       },
       connect : function(eventSource) {
-            var machine = new $.rta.eventMachine('worker');
-            machine.publish({
+            var bridge = new $.rta.communicationBridge('worker');
+            bridge.publish({
                 query : _private.getQuery(),
                 eventSource : eventSource
             });
-            machine.subscribe(function(e) {
+            bridge.subscribe(function(e) {
                 for (var sourceName in e.data) {
                     if (e.data[sourceName]) {
                         $(_private.subscriber[sourceName]).trigger('rta-message', e.data[sourceName]);
@@ -72,9 +71,9 @@ $.rta = (function() {
     };
 }());
 
-$.rta.eventMachine = function(adapter) {
+$.rta.communicationBridge = function(adapter) {
     var _private = {       
-        eventMachine : null,
+        communicationBridge : null,
         adapter : {
           'worker' : (function() {
               // The worker is encopsulated into worker.rta.js to show the experiment code
@@ -99,23 +98,23 @@ $.rta.eventMachine = function(adapter) {
           }
         }
     }    
-    _private.eventMachine = _private.adapter[adapter];
+    _private.communicationBridge = _private.adapter[adapter];
     // As browser window is closed, connection is closed as well
     $(window).unbind('unload').bind('unload', function() {
-       _private.eventMachine.terminate();
+       _private.communicationBridge.terminate();
     });
     // Not really neccessary, but useful when experimenting - Esc closes connection
     $(window).unbind('keypress').bind('keypress', function(e) {
        if (e.keyCode == 27) {
-           _private.eventMachine.terminate();
+           _private.communicationBridge.terminate();
        }
     });
     return {
         subscribe: function(callback) {
-            _private.eventMachine.subscribe(callback);
+            _private.communicationBridge.subscribe(callback);
         },
         publish : function(message) {
-            _private.eventMachine.publish(message);
+            _private.communicationBridge.publish(message);
         }
     }
 };
